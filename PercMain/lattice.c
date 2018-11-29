007f8d172f207f3db6bd2f3bf6b2e7deaf62cb2f
Dylan Johnson <dj.bytheriver@gmail.com>
2018-11-29 17:45:29 +0800

    Polished up the cache related code.

--- PercMain/lattice.c
+++ PercMain/lattice.c
@@ -1,6 +1,10 @@
 #include "percmain.h"
 
-#define LATTICE
+// Used when system information about cache lines can't be retrieved.
+int cacheDefault() {
+	printf("Unable to determine cache line size. Defaulting to 64 bytes.\n");
+	return 64;
+}
 
 // Obtain the size of cache lines on the host system.
 int getCacheSize() {
@@ -11,36 +15,39 @@ int getCacheSize() {
 	// This call is expected to fail, if it succeeds something weird has happened.
 	if (GetLogicalProcessorInformationEx(RelationCache, buffer, &bufferLength)) {
 		printf("Error finding buffer size for GetLogicalProcessorInformationEx(). Abort.\n");
-		exit(EXIT_FAILURE);
+		return cacheDefault();
 	}
 
 	// Now bufferLength should be set correctly, so allocate the buffer and make the call again.
 	buffer = (SYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX*)malloc(bufferLength);
 	if (buffer == NULL) {
 		printf("Buffer allocation for GetLogicalProcessorInformationEx() failed. Abort.\n");
-		exit(EXIT_FAILURE);
+		return cacheDefault();
 	}
 
 	// Get the desired information for real this time.
 	if (!GetLogicalProcessorInformationEx(RelationCache, buffer, &bufferLength)) {
 		printf("GetLogicalProcessorInformationEx() failed. Abort.\n");
-		exit(EXIT_FAILURE);
+		return cacheDefault();
 	}
 
 	// How many entries were placed in the buffer.
 	int numReturned = bufferLength / sizeof(SYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX);
+	int cache = -1;
 
 	// Finally, find the size of the L1 cache lines.
 	for (int i = 0; i < numReturned; i++) {
 		if (buffer[i].Cache.Level == 1) {
 			printf("Cache lines are %d bytes long.\n", buffer[i].Cache.LineSize);
-			return buffer[i].Cache.LineSize;
+			cache = buffer[i].Cache.LineSize;
+			break;
 		}
 	}
 
-	// If we got here, for some reason the cache lines couldn't be determined.
-	printf("Unable to determine cache line size. Defaulting to 64 bytes.\n");
-	return 64;
+	free(buffer);
+	
+	if (cache < 0) return cacheDefault();
+	else return cache;
 }
 
 // Create a lattice with size s and bond probability p.
