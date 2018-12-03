42188087f67734cc5e0372e3a4c320a3b95f0477
Dylan Johnson <dj.bytheriver@gmail.com>
2018-12-03 11:28:29 +0800

    Removed folders from old project, merged headers into one.

--- PercMain/percmain.h
+++ PercMain/percmain.h
@@ -1,5 +1,8 @@
 #pragma once
 
+// Frankly I'm unsure of the best way to structure includes for a project,
+// but this project is small enough that I can throw everything into one header.
+
 #include <windows.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -7,4 +10,38 @@
 #include <stdio.h>
 #include <time.h>
 #include <omp.h>
-#include "structures.h"
+
+// A Lattice is a 2D matrix of Sites.
+struct Site {
+	// Each bool represents a bond in a direction away from the site.
+	bool up, down, left, right;
+};
+
+struct Lattice {
+	// The length of one side of the lattice.
+	int size;
+	// Number of columns in a segment.
+	int segSize;
+	// The actual 2D matrix.
+	struct Site** xy;
+};
+
+// Rather than modify the lattice, search algorithms use Trackers to remember which Sites have already been traversed.
+struct Tracker {
+	bool** done;
+};
+
+// It's less hassle to store co-ordinates in a 1 dimensional array of structs.
+struct Vertex {
+	int x;
+	int y;
+};
+
+// Iterative DFS algorithms require a stack.
+struct Stack {
+	int size;
+	struct Vertex* data;
+};
+
+struct Lattice newLattice(int s, double p);
+void printLattice(struct Lattice lat);
